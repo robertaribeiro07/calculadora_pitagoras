c3ca0e8210a4849bb9325c28b463a4e3468835e5
robertaribeiro07 <49561000+robertaribeiro07@users.noreply.github.com>
2021-11-18 08:26:04 -0300

    Update views.py

--- calc_app/views.py
+++ calc_app/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 import math
 from .forms import MeuForm
 
+#função que calcula a hipotenusa
 def hipotenusa(request):
   form = MeuForm(request.POST or None)
   hipotenusa = '??????'
@@ -13,5 +14,6 @@ def hipotenusa(request):
       print(cat1, cat2)
       somacatetos = (cat1 * cat1) + (cat2 * cat2)
       hipotenusa = math.sqrt(somacatetos)
-    
+      
+  # Retorna o template html
   return render(request, 'pitagoras.html', {'form': form, 'hipotenusa': hipotenusa})
